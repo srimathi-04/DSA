023f6b457659703c2c6dc70869e1f21c6ea56547
srimathi-04 <94346565+srimathi-04@users.noreply.github.com>
2024-03-30 08:44:25 +0000

    Bn_S

---
+++ Binary Search Trees, Traversals and Recursion.py
@@ -0,0 +1,14 @@
+#QUESTION 1: As a senior backend engineer at Jovian, you are tasked with
+#developing a fast in-memory data structure to manage profile information
+#(username, name and email) for 100 million users. It should allow the following
+#operations to be performed efficiently:
+#1. Insert the profile information for a new user.
+#2. Find the profile information of a user, given their username
+#3. Update the profile information of a user, given their username
+#4. List all the users of the platform, sorted by username
+#You can assume that usernames are unique.
+
+
+
+
+
