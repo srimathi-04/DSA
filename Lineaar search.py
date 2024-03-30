a7ff329d51bb45e59400184d2c9b63fd53cac319
srimathi-04 <94346565+srimathi-04@users.noreply.github.com>
2024-03-30 08:16:29 +0000

    l_s

--- Lineaar search.py
+++ Lineaar search.py
@@ -1,35 +1,17 @@
-def binary_search(lo,hi,con):
-    while lo <= hi:
-        mid = (lo+hi)//2
-        res = con(mid)
-        if res == ' found':
-            return mid
-        elif res== 'left':
-            hi = mid-1
-        else:
-            lo = mid+1
-    return -1
-def first_position(nums,target):
-    def con(mid):
-        if nums[mid] == target:
-            if mid > 0 and nums[mid-1] == target:
-                return 'left'
-            return ' found'
-        elif nums[mid] <target:
-            return 'right'
-        else:
-            return ' left'
-    return binary_search(0,len(nums)-1,con)
-def last_position(nums,target):
-    def con(mid):
-        if nums[mid] == target:
-            if mid < len(nums)-1 and nums[mid+1] == target:
-                return 'right'
-            return ' found'
-        elif nums[mid] < target:
-            return 'right'
-        else:
-            return ' left'
-    return binary_search(0,len(nums)-1,con)
-def fir_last_pos(nums,target):
-    return first_position(nums,target),last_position(nums,target)
+
+def locate_cards(cards,query):
+  position = 0
+  print("position:",position)
+
+  while position < len(cards):
+    if cards[position] == query:
+      return position
+    position +=1
+  return -1
+cards=[]
+card = [19,18,14,13,12,7,3,1,5]
+query = 3
+print("cards:",cards)
+print("query:",query)
+print("position of the card is:",locate_cards(cards,query))
+print("position of the card is:",locate_cards(card,query))
